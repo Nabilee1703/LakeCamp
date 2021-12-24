3d3415dda88910f833e7388d23cbd5399a255e0d
Thu Trang <lthutrang17@gmail.com>
2021-12-25 00:39:14 +0700

    change name of schema

--- app.js
+++ app.js
@@ -83,8 +83,8 @@ const scriptSrcUrls = [
     "https://stackpath.bootstrapcdn.com/",
     "https://api.tiles.mapbox.com/",
     "https://api.mapbox.com/",
-    // "https://kit.fontawesome.com/",
-    // "https://cdnjs.cloudflare.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
     "https://cdn.jsdelivr.net",
 ];
 // const styleSrcUrls = [
@@ -96,12 +96,12 @@ const scriptSrcUrls = [
 //     "https://use.fontawesome.com/",
 // ];
 const styleSrcUrls = [
-    // "https://kit-free.fontawesome.com",
-    // "https://stackpath.bootstrapcdn.com",
+    "https://kit-free.fontawesome.com",
+    "https://stackpath.bootstrapcdn.com",
     "https://api.mapbox.com",
     "https://api.tiles.mapbox.com",
     "https://fonts.googleapis.com",
-    // "https://use.fontawesome.com",
+    "https://use.fontawesome.com",
     "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
 ];
 const connectSrcUrls = [
