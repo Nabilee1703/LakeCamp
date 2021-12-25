dd11e3dfbc32ac5998b3b65e3ec645693d34eb0d
Thu Trang <lthutrang17@gmail.com>
2021-12-25 16:47:13 +0700

    create file

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -57,8 +57,6 @@ const geoData = await geocoder.forwardGeocode({
     query: req.body.location,
        limit: 1
    }).send()
-
-const campground = await new CampGround(req.body)
 campground.geometry = geoData.body.features[0].geometry;
    const imgs = req.files.map(f =>({path: f.path, filename: f.filename}))
    campground.images.push(...imgs)
